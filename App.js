2aacd1354877df06cea1fb42bb274742cd39397d
TeemuRossi <33146833+TeemuRossi@users.noreply.github.com>
2019-12-06 18:58:09 +0200

    Update App.js

--- App.js
+++ App.js
@@ -2,35 +2,55 @@ import React , {useEffect, useState} from "react";
 import Thenews from './bringNews';
 
 
-const App = () => {
-  const url= 'https://newsapi.org/v2/everything?q=bitcoin&apiKey=36dd4792425040a1b74b821ffc3cb4d2';
-  const [news, setnews] = useState([]);
-  
+export const App = () => {
+
+  const [news, setNews] = useState([]);
+  const [search, setSearch] = useState('');
+  const [query, setQuery] = useState("Bitcoin");
+
   useEffect(()=> {
     getNews();
-  }, []);
+  }, [query]);
 
   const getNews = async () => {
-    const response = await fetch(url);
+    const response = await fetch(`
+    https://newsapi.org/v2/everything?q=${query}&apiKey=36dd4792425040a1b74b821ffc3cb4d2`);
     const data = await response.json();
-    setnews(data.articles);
+    setNews(data.articles);
     console.log(data.articles);
   };
+
+  const updateSearch = e => {
+    setSearch(e.target.value);
+    console.log(search);
+  };
+  
+  const getSearch = e => {
+    e.preventDefault();
+    setQuery(search);
+  };
+  
   return (
     <div className="App">
-      <form className="search-form">
-        <input className="search-bar" type="search"/>
-        <button onclick= {getNews}className="search-button" type="submit">
+      <form onSubmit={getSearch} className="search-form">
+        <input className="search-bar" type="search" value={search} onChange={updateSearch}/>
+        <button className="search-button" type="submit">
           Search!
         </button>
-        
-
       </form>
-      {news.map(Thenews=>(
-        <Thenews/>
-    ))}
+        {news.map(news=>(
+          <Thenews 
+          key={news.title}
+          title={news.title}
+          image={news.urlToImage}
+          author={news.author}
+          description={news.description}
+                    />))}
+
+      
+      
+    
     </div>
   );
   
 };
-export default App;
