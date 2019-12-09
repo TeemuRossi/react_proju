a3bccce805d2ba11f6ebca442c9fc7832ab826dc
TeemuRossi <33146833+TeemuRossi@users.noreply.github.com>
2019-12-09 23:27:08 +0200

    adding fields to change the results

--- App.js
+++ App.js
@@ -7,14 +7,17 @@ export const App = () => {
   const [news, setNews] = useState([]);
   const [search, setSearch] = useState('');
   const [query, setQuery] = useState("Bitcoin");
-
+  const [country, setCountry] = useState("");
+  const [endpoint, setEndpoint] = useState("everything")
+  
   useEffect(()=> {
     getNews();
-  }, [query]);
-
+  }, [query,country,endpoint]);
+  
   const getNews = async () => {
-    const response = await fetch(`
-    https://newsapi.org/v2/everything?q=${query}&apiKey=36dd4792425040a1b74b821ffc3cb4d2`);
+
+    const response = await fetch(`https://newsapi.org/v2/${endpoint}?${country}q=${query}&apiKey=36dd4792425040a1b74b821ffc3cb4d2`);
+    //https://newsapi.org/v2/everything?q=${query}&sortBy=popularity&apiKey=36dd4792425040a1b74b821ffc3cb4d2
     const data = await response.json();
     setNews(data.articles);
     console.log(data.articles);
@@ -22,22 +25,46 @@ export const App = () => {
 
   const updateSearch = e => {
     setSearch(e.target.value);
-    console.log(search);
+    
   };
   
   const getSearch = e => {
     e.preventDefault();
     setQuery(search);
+    
   };
-  
+  const updateCountry = e => {
+    setCountry(e.target.value);
+    
+  };
+  const updateEndpoint = e => {
+    setEndpoint(e.target.value);
+    console.log(endpoint);
+  };
+
   return (
     <div className="App">
-      <form onSubmit={getSearch} className="search-form">
-        <input className="search-bar" type="search" value={search} onChange={updateSearch}/>
-        <button className="search-button" type="submit">
-          Search!
-        </button>
-      </form>
+      <div className="search">
+        <form onSubmit={getSearch} className="search-form">
+            <input className="search-bar" type="search" value={search} onChange={updateSearch}/>
+            <input type="radio" name="endpoint" value="everything"onChange={updateEndpoint}/>  All news
+            <input type="radio" name="endpoint" value="top-headlines"onChange={updateEndpoint}/> Top Headlines
+          <select className="country-select" value={country} onChange={updateCountry} >
+              <option value="">All countries</option>
+              <option value="country=us&">United states</option>
+              <option value="country=gb&">Great Britain</option>
+              <option value="country=de&">Germany</option>
+              <option value="country=de&">France</option>
+              <option value="country=jp&">Japan</option>
+            </select>
+          <button className="search-button" type="submit">
+            Search!
+          </button>
+        </form>
+      
+      
+        
+      </div>
         {news.map(news=>(
           <Thenews 
           key={news.title}
