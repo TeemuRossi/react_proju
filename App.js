0a56f9aa6937d745eadcbe0192fa3e498b2532d3
TeemuRossi <33146833+TeemuRossi@users.noreply.github.com>
2019-12-10 18:11:16 +0200

    added getnews to useEffect and link
    This removes the warning about "React Hook useEffect has a missing dependency".
    Added link aswell as a new element to the data fetched

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 import React , {useEffect, useState} from "react";
 import Thenews from './bringNews';
-
+import "./App.css";
 
 export const App = () => {
 
@@ -10,23 +10,25 @@ export const App = () => {
   const [endpoint, setEndpoint] = useState("everything")
   
   useEffect(()=> {
+    const getNews = async () => {
+
+      const response = await fetch(`https://newsapi.org/v2/${endpoint}?q=${query}&apiKey=36dd4792425040a1b74b821ffc3cb4d2`);
+      //https://newsapi.org/v2/everything?q=${query}&sortBy=popularity&apiKey=36dd4792425040a1b74b821ffc3cb4d2
+      const data = await response.json();
+      
+      setNews(data.articles);
+      console.log(data.articles);
+      
+      
+      
+    };
+    
     getNews();
   }, [query,endpoint]);
   
   
   
-  const getNews = async () => {
-
-    const response = await fetch(`https://newsapi.org/v2/${endpoint}?q=${query}&apiKey=36dd4792425040a1b74b821ffc3cb4d2`);
-    //https://newsapi.org/v2/everything?q=${query}&sortBy=popularity&apiKey=36dd4792425040a1b74b821ffc3cb4d2
-    const data = await response.json();
-    
-    setNews(data.articles);
-    console.log(data.articles);
-    
-    
-    
-  };
+  
 
   const updateSearch = e => {
     setSearch(e.target.value);
@@ -36,30 +38,30 @@ export const App = () => {
   const getSearch = e => {
     e.preventDefault();
     setQuery(search);
-    
+    setSearch("");
   };
   
   const updateEndpoint = e => {
     setEndpoint(e.target.value);
     console.log(endpoint);
+    
   };
 
   return (
     <div className="App">
-      <div className="search">
         <form onSubmit={getSearch} className="search-form">
-            <input className="search-bar" type="search" value={search} onChange={updateSearch} placeholder="bitcoin"/>
-            <input type="radio" name="endpoint" value="everything"onChange={updateEndpoint}/>  All news
-            <input type="radio" name="endpoint" value="top-headlines"onChange={updateEndpoint}/> Top Headlines
-           
+          <input className="search-bar" type="search" value={search} onChange={updateSearch} placeholder="Search "/>
           <button className="search-button" type="submit">
-            Search!
+            search
           </button>
         </form>
       
+      <div className="radio">
+        <input type="radio" name="endpoint" value="everything"onChange={updateEndpoint}/>  All news (default)
+        <input type="radio" name="endpoint" value="top-headlines"onChange={updateEndpoint}/> Top Headlines
       
-        
       </div>
+      <div className="news">
         {news.map(news=>(
           <Thenews 
           key={news.description}
@@ -67,7 +69,11 @@ export const App = () => {
           image={news.urlToImage}
           author={news.author}
           description={news.description}
-                    />))}
+          link={news.url}
+          
+
+          />))}
+     </div>
 
       
       
