105f8f5cee0ebdc2b9cfa496b6ad8d35d0871451
TeemuRossi <33146833+TeemuRossi@users.noreply.github.com>
2019-12-10 09:45:38 +0200

    deleted country change

--- App.js
+++ App.js
@@ -7,20 +7,25 @@ export const App = () => {
   const [news, setNews] = useState([]);
   const [search, setSearch] = useState('');
   const [query, setQuery] = useState("Bitcoin");
-  const [country, setCountry] = useState("");
   const [endpoint, setEndpoint] = useState("everything")
   
   useEffect(()=> {
     getNews();
-  }, [query,country,endpoint]);
+  }, [query,endpoint]);
+  
+  
   
   const getNews = async () => {
 
-    const response = await fetch(`https://newsapi.org/v2/${endpoint}?${country}q=${query}&apiKey=36dd4792425040a1b74b821ffc3cb4d2`);
+    const response = await fetch(`https://newsapi.org/v2/${endpoint}?q=${query}&apiKey=36dd4792425040a1b74b821ffc3cb4d2`);
     //https://newsapi.org/v2/everything?q=${query}&sortBy=popularity&apiKey=36dd4792425040a1b74b821ffc3cb4d2
     const data = await response.json();
+    
     setNews(data.articles);
     console.log(data.articles);
+    
+    
+    
   };
 
   const updateSearch = e => {
@@ -33,10 +38,7 @@ export const App = () => {
     setQuery(search);
     
   };
-  const updateCountry = e => {
-    setCountry(e.target.value);
-    
-  };
+  
   const updateEndpoint = e => {
     setEndpoint(e.target.value);
     console.log(endpoint);
@@ -46,17 +48,10 @@ export const App = () => {
     <div className="App">
       <div className="search">
         <form onSubmit={getSearch} className="search-form">
-            <input className="search-bar" type="search" value={search} onChange={updateSearch}/>
+            <input className="search-bar" type="search" value={search} onChange={updateSearch} placeholder="bitcoin"/>
             <input type="radio" name="endpoint" value="everything"onChange={updateEndpoint}/>  All news
             <input type="radio" name="endpoint" value="top-headlines"onChange={updateEndpoint}/> Top Headlines
-          <select className="country-select" value={country} onChange={updateCountry} >
-              <option value="">All countries</option>
-              <option value="country=us&">United states</option>
-              <option value="country=gb&">Great Britain</option>
-              <option value="country=de&">Germany</option>
-              <option value="country=de&">France</option>
-              <option value="country=jp&">Japan</option>
-            </select>
+           
           <button className="search-button" type="submit">
             Search!
           </button>
@@ -67,7 +62,7 @@ export const App = () => {
       </div>
         {news.map(news=>(
           <Thenews 
-          key={news.title}
+          key={news.description}
           title={news.title}
           image={news.urlToImage}
           author={news.author}
