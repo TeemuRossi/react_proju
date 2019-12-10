36bfeb7e8bc05630658ebf99429da0c39b2e0f11
TeemuRossi <33146833+TeemuRossi@users.noreply.github.com>
2019-12-10 18:13:09 +0200

    Added some style elements
    added style to the elements, trying out the css module, it still gives errors about true value in classname....

--- bringNews.js
+++ bringNews.js
@@ -1,17 +1,24 @@
 import React from 'react';
+import style from './Thenews.module.css';
 
-
-const Thenews = ({title,image,author,description}) => {
-    return(
-      <div>
-        <h1>{title}</h1>
-        <img src={image} alt="OOPPS"/>
-        <h2>{author}</h2>
-        <p>{description}</p>
-
-      </div>
+const Thenews = ({title,image,author,description,link}) => {
+ return(
+        <div className={style.news}>
+          <h1 className={style.title}>{title}</h1>
+          <h4 className={style.author}>Author: {author}</h4>
+          <img className={style.image} src={image} alt="OOPPS there seems to be nothing here"/>
+          <p className={style.description}>{description}</p>
+          <div className={style.link}>
+            <a className href={link} target="_blank" rel="noopener noreferrer">Read more...</a>
+          </div>
+        </div>);
+     
+      
+  
         
   
-      );
-}
+      
+  }
+  
+
 export default Thenews;
